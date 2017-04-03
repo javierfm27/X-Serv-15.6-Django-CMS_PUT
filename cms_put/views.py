aa5d54578f18121ecc52e24917fbd0627d936e5e
javierfm27 <javitofm27@github.com>
2017-04-03 19:54:09 +0200

    Ejercicio Inacabado

--- cms_put/views.py
+++ cms_put/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from cms_put.models import Pages
 
 # Create your views here.
+@csrf_exempt #SE UTILIZA PARA QUE NOS PERMITA REALIZAR EL POST
 def main(request):
     if request.method == 'GET':
-        return HttpResponse("Hacemos un GET")
-    elif request.method == 'POS':
-        return HttpResponse("Hacemos un POST")
+        htmlAnswer = "<form id='paginas' method='POST'>" \
+            + "<label> Introduce el recurso y el contenido del recurso</br></label>" \
+            + "<input name='name' type='text'>" \
+            + "<br>" \
+            + "<textarea name='page' rows='20' cols='100' ></textarea>" \
+            + "<br>" \
+            + "<input type='submit' value='Enviar'></form>"
+        return HttpResponse(htmlAnswer)
+    elif request.method == 'PUT':
+        return HttpResponse("Hacemos un PUT")
+    elif request.method == 'POST':
+        recurso = request.POST['name']
+        contenido = request.POST['page']
+        pagina = Pages(name=recurso, page=contenido)
+        pagina.save()
+        return HttpResponse("Hacemos un POST" + request.body.decode('utf-8'))
 
-
-def mainPut(request):
-    htmlAnswer = "<form id='paginas' method='put'>" \
-        + "<label> Introduce el recurso y el contenido del recurso</br></label>" \
-        + "<input name='recurso' type='text'>" \
-        + "<input name='contenido' type='text'>" \
-        + "<input type='submit' value='Enviar'></form>"
-    return HttpResponse(htmlAnswer)
+def recurso(request, nombreRecurso):
+    if request.method == 'GET':
+        pagina = Pages.objects.get(name=nombreRecurso)
+        return HttpResponse(pagina.page)
--- project/urls.py
+++ project/urls.py
@@ -17,8 +17,7 @@ from django.contrib import admin
 from cms_put import views
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'', views.mainPut,name='Pagina que habilita hacer un Put'),
-    url(r'^.*',views.main,name='Pagina que devuelve lo que se ha introducido \
-        con Poster'),
+    url(r'^admin', include(admin.site.urls)),
+    url(r'(.+)', views.recurso, name='Se esta haciendo un GET de algo guardado'),
+    url(r'.*', views.main,name='Pagina que habilita hacer un POST'),
 ]
